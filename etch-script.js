096a1fa7fe23dd22f8fd19ca01c58a491b529416
lmfrench <lisa_french@hotmail.com>
2023-05-03 20:51:20 +0100

    Add gridSelect button

--- etch-script.js
+++ etch-script.js
@@ -15,14 +15,46 @@ function createGrid(col,row){
     
 }
 
+function gridSelect(){
+    //remove previous grid
+    document.querySelectorAll('.box').forEach(e => e.remove());
+    
+    let userSelect = prompt("How many rows?");
+    
+    validSelection = false;
+    while (validSelection !==true){
+        //check number was entered
+        if (isNaN(userSelect)){
+            userSelect = prompt("You must enter a number. How many rows?");
+        }
+        //check not higher than 100
+        else if (userSelect >100){
+            userSelect = prompt("You must enter less than 100. How many rows?");
+        }
+        
+        else {
+            validSelection = true;
+        }
+    }
+    
+        return userSelect;
+}
 
-createGrid(16,16);
-
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach((box) => {
+//Event listener on button to ask user for grid size
+const button = document.getElementById('select');
+button.addEventListener('click', () =>{
+    let gridSize = gridSelect();
+    createGrid(gridSize,gridSize);
+    
+    //Event listener to change color on hoverover
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
     box.addEventListener('mouseover', () => {
         box.style.cssText = "background-color: white";
     });
 });
+
+});
+
+
 
